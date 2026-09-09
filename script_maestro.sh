6116457c4b8dd000e0831eb473df7755f83a5e6c
Jhonny Jiménez <MasterChampionLigue@gmail.com>
2026-09-09 01:03:32 -0600

    Script maestro: limpieza de paquetes no deseados. Renombre de alias.

--- script_maestro.sh
+++ script_maestro.sh
@@ -2,6 +2,12 @@
 
 # Este script contendrá todo aquello que quiero ejecutar en mi computadora porque tener cientos de scripts pequeños me parece una lata.
 
+ENTORNO="$REPO/librerias/info.env"
+
+if [[ -f "$ENTORNO" ]]; then
+    source "$ENTORNO"
+fi
+
 nt() {
 	N_ScriptName=${3:-(basename "$0")}
 	args=("-i" "$2" "$N_ScriptName" "$1")
@@ -29,6 +35,48 @@ case $1 in
 				echo "$mensaje"
 				notificación "$mensaje" "/usr/share/icons/Papirus/128x128/apps/distributor-logo-archlinux.svg"
 				exit 1
+			;;
+		esac
+	;;
+# ------------------------------------------------------------------------------
+# Sección 2: Consola.
+	'consola')
+		case "$2" in
+			reiniciar-audio)
+				systemctl --user stop wireplumber.service
+				# systemctl --user start wireplumber.service
+				sleep 2
+				pactl set-sink-volume @DEFAULT_SINK@ 67%
+			;;
+			limpiar-actualización)
+				gestor="yay"
+
+				if ! (command -v "$gestor" &>/dev/null); then
+					gestor="pacman"
+				fi
+
+				paquetes_listados=("go" "yay-debug" "oh-my-posh-git-debug" "cubeb-debug" "zahar-git-debug" "nomacs-debug" "catch2" "aimp-debug" "iwgtk-debug" "jq" "oniguruma" "adwaita-fonts" "gnu-free-fonts" "ttf-jetbrains-mono")
+
+				huerfanos=()
+				mapfile -t huerfanos < <($gestor -Qdttq 2>/dev/null)
+				paquetes_listados+=("${huerfanos[@]}")
+
+				paquetes_a_borrar=()
+				for paquete in "${paquetes_listados[@]}"; do
+					if $gestor -Qq "$paquete" &>/dev/null; then
+						paquetes_a_borrar+=("$paquete")
+					fi
+				done
+
+				if [ ${#paquetes_a_borrar[@]} -gt 0 ]; then
+					if [[ "$gestor" == "pacman" ]]; then
+						echo "$contra" | sudo -S pacman -Rddns --noconfirm "${paquetes_a_borrar[@]}"
+					else
+						$gestor -Rddns --noconfirm "${paquetes_a_borrar[@]}"
+					fi
+				else
+					echo "No hay paquetes por eliminar instalados."
+				fi
 			;;
 		esac
 	;;
